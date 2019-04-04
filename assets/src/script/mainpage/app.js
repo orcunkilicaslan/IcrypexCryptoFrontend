85be2219930a731a40ae61f4a081bba89aa0f175
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-04-04 18:00:43 +0300

    Homepage Header Top

--- assets/src/script/mainpage/app.js
+++ assets/src/script/mainpage/app.js
@@ -1,3 +1,15 @@
+/* Homepage Slider */
+$('#homepageslider').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    items:1,
+    margin:10,
+    animateOut: 'fadeOut'
+});
+/* Homepage Slider */
 
 /* Loading Info */
 console.log("Mainpage App Javascript Loading Successful");
