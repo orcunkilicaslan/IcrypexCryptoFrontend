b2e1540334b7220f71cd17a629657ee49cf8c17f
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-04-19 14:48:18 +0300

    Deposit Step 2 Bank 1 Finish

--- assets/src/script/tradepage/app.js
+++ assets/src/script/tradepage/app.js
@@ -106,28 +106,6 @@ $('#headerbottompricelite').owlCarousel({
 /* Header Bottom Lite Price */
 
 /* Deposit and Withdraw */
-$(document).ready(function() {
-    $('#depositwizard').bootstrapWizard({onNext: function(tab, navigation, index) {
-            if(index==2) {
-                // Make sure we entered the name
-                if(!$('#name').val()) {
-                    alert('You must enter your name');
-                    $('#name').focus();
-                    return false;
-                }
-            }
-
-            // Set the name for the next tab
-            $('#tab3').html('Hello, ' + $('#name').val());
-
-        }, onTabShow: function(tab, navigation, index) {
-            var $total = navigation.find('li').length;
-            var $current = index+1;
-            var $percent = ($current/$total) * 100;
-            $('#depositwizard .progress-bar').css({width:$percent+'%'});
-        }});
-    window.prettyPrint && prettyPrint()
-});
 /* Deposit and Withdraw */
 
 
