4c3fbc06e496b2baaf25c8fbeb50552ef6a58cbc
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-04-29 13:48:42 +0300

    Profile Account Confirmation Finish

--- assets/src/script/tradepage/app.js
+++ assets/src/script/tradepage/app.js
@@ -18,6 +18,34 @@ Dropzone.options.profileKimlikOnYuz = {
     paramName: "file", // The name that will be used to transfer the file
     dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
 };
+Dropzone.options.profileKimlikArkaYuz = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileDilekceliFotograf = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileAdresTeyidi = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileBankaDokumu = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileCalismaBelgesi = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileBankaReferansi = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
+Dropzone.options.profileEkBelge = {
+    paramName: "file", // The name that will be used to transfer the file
+    dictDefaultMessage: "<i class='icon-svg icon-svg-profilefileuploaddragdrop'></i>" + "SÜRÜKLE & BIRAK" + "<br />" + "[JPG, PNG, PDF-MAKS 5MB]"
+};
 /* DropZone JS */
 
 
