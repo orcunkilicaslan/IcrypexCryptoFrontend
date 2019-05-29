7b63b808f8b03b92e60e2cf20481e541f79e64fa
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-05-29 13:47:30 +0300

    Profile Login Settings Add Range Slider

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -31,29 +31,6 @@ $(function() {
 });
 /* Navbar Menu */
 
-/* Range Slider */
-var rangeSlider = function(){
-    var slider = $('.range-slider'),
-        range = $('.range-slider__range'),
-        value = $('.range-slider__value');
-
-    slider.each(function(){
-
-        value.each(function(){
-            var value = $(this).prev().attr('value');
-            $(this).html(value);
-        });
-
-        range.on('input', function(){
-            $(this).next(value).html(this.value);
-        });
-    });
-};
-
-rangeSlider();
-/* Range Slider */
-
-
 /* Loading Info */
 console.log("Plugin App Javascript Loading Successful");
 /* Loading Info */
