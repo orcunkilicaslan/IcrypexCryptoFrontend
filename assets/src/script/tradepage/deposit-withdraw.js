192b96f947d639f7a1c13ebff78e35e0bcd8133d
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-07-10 12:47:02 +0300

    Deposit-Withdraw Checkbox Button Validation Fixed

--- assets/src/script/tradepage/deposit-withdraw.js
+++ assets/src/script/tradepage/deposit-withdraw.js
@@ -1,44 +1,27 @@
 /* Deposit-Withdraw Checkbox Button Validation */
 jQuery(function($) {
-    if($('.stepwizard-tabcont-form').hasClass('formuinew')){
-        window.onload = checkedCheckboxOnPageLoad();
-        var WinzardiReadCheck = document.getElementById("WinzardiReadCheck").checked;
-
-        function checkedCheckboxOnPageLoad() {
+    if($('.stepwizard-validation').hasClass('formlabelvalidation')){
+        var WinzardiReadCheck = $('#WinzardiReadCheck').is(':checked');
+        window.onload = function(e){
             if(!WinzardiReadCheck){
-                $('#WinzardBottomButtonArea').tooltip({container: ".stepwizard-tabcont-form-btn-tooltiparea", title: ' "Okudum, anladım" İşaretleyiniz ', placement: 'top'});
-                document.getElementById("WinzardBottomButtonDiv").classList.add('secondary');
-                document.getElementById("WinzardBottomButtonDiv").classList.remove('success');
-                document.getElementById("WinzardBottomButtonLink").classList.add('btn-secondary');
-                document.getElementById("WinzardBottomButtonLink").classList.remove('btn-success');
-                document.getElementById("WinzardBottomButtonLink").disabled = true;
+                $('#WinzardBottomButtonArea').tooltip({container: '.stepwizard-tabcont-form-btn-tooltiparea', title: ' "Okudum, anladım" İşaretleyiniz ', placement: 'top'}).addClass('disabled');
+                $('#WinzardBottomButtonDiv').addClass('secondary').removeClass('success');
+                $('#WinzardBottomButtonLink').addClass('btn-secondary').removeClass('btn-success').attr("disabled", true);
             }
-        };
+        }
     }
 });
 
-$(document).ready(function(){
-    $("#WinzardiReadCheck").click(function(){
-        if($("#WinzardiReadCheck").is(':checked')){
-            $(document).ready(function() {
-                $('#WinzardBottomButtonArea').tooltip('disable');
-                document.getElementById("WinzardBottomButtonDiv").classList.remove('secondary');
-                document.getElementById("WinzardBottomButtonDiv").classList.add('success');
-                document.getElementById("WinzardBottomButtonLink").classList.remove('btn-secondary');
-                document.getElementById("WinzardBottomButtonLink").classList.add('btn-success');
-                document.getElementById("WinzardBottomButtonLink").disabled = false;
-            });
-        }else{
-            $(document).ready(function() {
-                $('#WinzardBottomButtonArea').tooltip({container: ".stepwizard-tabcont-form-btn-tooltiparea", title: ' "Okudum, anladım" İşaretleyiniz ', placement: 'top'});
-                document.getElementById("WinzardBottomButtonDiv").classList.add('secondary');
-                document.getElementById("WinzardBottomButtonDiv").classList.remove('success');
-                document.getElementById("WinzardBottomButtonLink").classList.add('btn-secondary');
-                document.getElementById("WinzardBottomButtonLink").classList.remove('btn-success');
-                document.getElementById("WinzardBottomButtonLink").disabled = true;
-            });
-        }
-    });
+$("#WinzardiReadCheck").click(function(e){
+    if(($(this)).is(':checked')){
+        $('#WinzardBottomButtonArea').tooltip('disable').removeClass('disabled');;
+        $('#WinzardBottomButtonDiv').addClass('success').removeClass('secondary');
+        $('#WinzardBottomButtonLink').addClass('btn-success').removeClass('btn-secondary').attr("disabled", false);
+    }else{
+        $('#WinzardBottomButtonArea').tooltip('enable', {container: '.stepwizard-tabcont-form-btn-tooltiparea', title: ' "Okudum, anladım" İşaretleyiniz ', placement: 'top'}).addClass('disabled');
+        $('#WinzardBottomButtonDiv').addClass('secondary').removeClass('success');
+        $('#WinzardBottomButtonLink').addClass('btn-secondary').removeClass('btn-success').attr("disabled", true);
+    }
 });
 /* Deposit-Withdraw Checkbox Button Validation */
 
