947b24e09a7cb500121761ca9f004d47f9ea8143
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-07-12 10:51:36 +0300

    Profile Profile Add Phone Mask

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -89,6 +89,7 @@ jQuery(function($) {
     } else {
         $(document).ready(function() {
             $(".header-userarea-user").click(function() {
+                $("#MainpageDropdownMenu").addClass("activeuserdropdownmenu");
                 $("#TradepageDropdownMenu").addClass("activeuserdropdownmenu");
             });
         });
@@ -101,6 +102,13 @@ $(function(){
     $('[data-toggle="tooltip"]').tooltip()
 });
 /* Tooltip */
+
+/* Input Mask */
+jQuery(function($){
+    $(".inputdatemask").mask("99/99/9999",{placeholder:"MM/DD/YYYY"});
+    $(".inputphonemask").mask("0599 999 99 99",{placeholder:"05__ ___ __ __"});
+});
+/* Input Mask */
 
 /* Loading Info */
 console.log("Plugin App Javascript Loading Successful");
