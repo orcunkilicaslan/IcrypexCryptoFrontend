fc810c37f14142403b077b9d729667801ec398fd
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-08-01 19:34:05 +0300

    Homepage 400px Responsive Fixed

--- assets/src/script/mainpage/responsive-app.js
+++ assets/src/script/mainpage/responsive-app.js
@@ -63,7 +63,7 @@ jQuery(function($) {
                 loop:true,
                 nav:true,
                 dots:false,
-                autoplay:false,
+                autoplay:true,
                 autoplayTimeout:3000,
                 autoplayHoverPause:true,
                 items:2,
