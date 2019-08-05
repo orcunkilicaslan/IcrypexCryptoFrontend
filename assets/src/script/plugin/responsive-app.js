365de11c6fb9ffa66297f79b90ce80ca44d63dde
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-08-05 11:02:49 +0300

    Homepage Responsive User Logged In Menu Fixed

--- assets/src/script/plugin/responsive-app.js
+++ assets/src/script/plugin/responsive-app.js
@@ -1,11 +1,49 @@
 
-/* Responsive */
+/* Responsive Header User Dropdown Menu List */
 jQuery(function($) {
-    if($(window).width() > 767){
+    if($(window).width() < 767){
+        jQuery(function($) {
+            if($('body').hasClass('mainpage')){
+                $(document).ready(function() {
 
+                    $("#MobileMenuUserDropdownMenu").on("click", function (){
+
+                        var userMenuList = $('#MobileMenuUserDropdownMenu .headusermenu-menulist');
+                        var dropdownMobileTradepageMenu = $('#MobileMenuTradepageDropdownMenu');
+
+                        if (userMenuList.height() == 0) {
+                            userMenuList.animate({height: dropdownMobileTradepageMenu.height()+15});
+                            dropdownMobileTradepageMenu.addClass("activeuserdropdownmenu");
+                        }
+                        else if (userMenuList.height() == dropdownMobileTradepageMenu.height()+15) {
+                            userMenuList.animate({height: "0px"});
+                        }
+                    });
+
+                });
+            }else if($('body').hasClass('tradepage')){
+                $(document).ready(function() {
+
+                    $("#MobileMenuUserDropdownMenu").on("click", function (){
+
+                        var userMenuList = $('#MobileMenuUserDropdownMenu .headusermenu-menulist');
+                        var dropdownMobileMainpageMenu = $('#MobileMenuMainpageDropdownMenu');
+
+                        if (userMenuList.height() == 0) {
+                            userMenuList.animate({height: dropdownMobileMainpageMenu.height()+15});
+                            dropdownMobileMainpageMenu.addClass("activeuserdropdownmenu");
+                        }
+                        else if (userMenuList.height() == dropdownMobileMainpageMenu.height()+15) {
+                            userMenuList.animate({height: "0px"});
+                        }
+                    });
+
+                });
+            }
+        });
     }
 });
-/* Responsive */
+/* Responsive Header User Dropdown Menu List */
 
 /* Loading Info */
 // console.log("Plugin App Mobile Responsive Javascript Loading Successful");
