bbb857460e01935157c58cea7da0c5b06f876352
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-08-29 13:01:30 +0300

    Tradepage Responsive JS Added

---
+++ assets/src/script/tradepage/responsive-app.js
@@ -0,0 +1,34 @@
+
+/* Responsive Tradepage Profile Menu Dropdown */
+jQuery(function($) {
+    if($(window).width() <= 991){
+        $(document).ready(function($){
+
+            var sidebarProfileMenuLocation              = $('.sidebar-tradepage');
+            var sidebarProfileMenuNavigation            = $('.sidebar-navigasyon');
+            var sidebarProfileMenuNavigationAccount     = $('.sidebar-navigasyon-account');
+            var sidebarProfileMenuNavigationSecurity    = $('.sidebar-navigasyon-security');
+
+            var sidebarProfileMenuNavigationAccountActiveMenuText       = sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationAccount).find('ul.nav .sidebar-navigasyon-list-item').find('.active').text();
+            var sidebarProfileMenuNavigationSecurityActiveMenuText      = sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationSecurity).find('ul.nav .sidebar-navigasyon-list-item').find('.active').text();
+
+            // Dropdown Menu Active Class Menu Remove
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationAccount).find('ul.nav').find('.active').parent().remove();
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationSecurity).find('ul.nav').find('.active').parent().remove();
+
+            // Dropdown Menu Add Class
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationAccount).find('ul.nav').addClass('dropdown-menu responsive-dropdown-menu responsive-dropdown-top-menu');
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationSecurity).find('ul.nav').addClass('dropdown-menu responsive-dropdown-menu responsive-dropdown-bottom-menu');
+
+            // Dropdown Button Added
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationAccount).append($('<a class="responsive-dropdown-btn responsive-dropdown-top-btn btn" href="#" data-toggle="dropdown">' + 'HESABIM' + '</a>'));
+            sidebarProfileMenuLocation.find(sidebarProfileMenuNavigationSecurity).append($('<a class="responsive-dropdown-btn responsive-dropdown-bottom-btn btn" href="#" data-toggle="dropdown">' + 'GÜVENLİK' + '</a>'));
+
+        });
+    }
+});
+/* Responsive Tradepage Profile Menu Dropdown */
+
+/* Loading Info */
+// console.log("Tradepage App Responsive Javascript Loading Successful");
+/* Loading Info */
