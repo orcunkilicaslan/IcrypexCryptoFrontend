0c8321fabeb45dfe5c99bb47f7a388b9f7a04bb4
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-01-03 14:25:50 +0300

    Login Step 2 SMS Changed and Input Filter

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -185,6 +185,46 @@ $(document).ready(function(){
 });
 /* Input Mask */
 
+/* Input Filter (https://jsfiddle.net/emkey08/tvx5e7q3) */
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    };
+}(jQuery));
+
+
+// Install Input Filter
+$(".inputfilter-integer").inputFilter(function(value) {
+    return /^-?\d*$/.test(value);
+});
+$(".inputfilter-limitinteger").inputFilter(function(value) {
+    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 500);
+});
+$(".inputfilter-floatinteger").inputFilter(function(value) {
+    return /^-?\d*[.,]?\d*$/.test(value);
+});
+$(".inputfilter-currency").inputFilter(function(value) {
+    return /^-?\d*[.,]?\d{0,2}$/.test(value);
+});
+$(".inputfilter-latinonly").inputFilter(function(value) {
+    return /^[a-z]*$/i.test(value);
+});
+$(".inputfilter-hexadecimal").inputFilter(function(value) {
+    return /^[0-9a-f]*$/i.test(value);
+});
+/* Input Filter */
+
 /* Footer Fresh Chat */
 $(window).on("load resize",function(e){
     if($(window).width() >= 768){
