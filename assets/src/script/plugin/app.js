7967a9a39910b81b4f50854f47f1475cff3e8580
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-08-19 17:11:00 +0300

    Livechat Icon Fixed

--- assets/src/script/plugin/app.js
+++ assets/src/script/plugin/app.js
@@ -244,13 +244,13 @@ $(window).on("load resize",function(e){
     if($(window).width() >= 768){
 
         if(!$('.cookies-notification').hasClass('show')){
-            $("#sc924I > .psmw_924I").css("bottom", ("5")+"px");
+            $("#sc924I > div[style*='fixed']").css("bottom", ("5")+"px");
         } else {
-            $("#sc924I > .psmw_924I").css("bottom", ("71")+"px");
+            $("#sc924I > div[style*='fixed']").css("bottom", ("71")+"px");
         }
 
         $(".cookies-notification a.notificationclose").click(function() {
-            $("#sc924I > .psmw_924I").css("bottom", ("5")+"px");
+            $("#sc924I > div[style*='fixed']").css("bottom", ("5")+"px");
         });
     }
 });
