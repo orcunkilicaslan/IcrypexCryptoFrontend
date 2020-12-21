b4ac42f78968dea1c9268a011e017ac50acefed4
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2020-12-22 00:37:17 +0300

    GulpFile Fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,28 @@
-const   gulp                      = require('gulp'),
-        del                       = require('del'),
-        sourcemaps                = require('gulp-sourcemaps'),
-        plumber                   = require('gulp-plumber'),
-        sass                      = require('gulp-sass'),
-        autoprefixer              = require('gulp-autoprefixer'),
-        cssnano                   = require('gulp-cssnano'),
-        babel                     = require('gulp-babel'),
-        webpack                   = require('webpack-stream'),
-        uglify                    = require('gulp-uglify'),
-        concat                    = require('gulp-concat'),
-        browserSync               = require('browser-sync').create(),
+const   gulp                        = require('gulp'),
+        del                         = require('del'),
+        gulppif                     = require('gulp-if'),
+        util                        = require('gulp-util'),
+        sourcemaps                  = require('gulp-sourcemaps'),
+        plumber                     = require('gulp-plumber'),
+        sass                        = require('gulp-sass'),
+        autoprefixer                = require('gulp-autoprefixer'),
+        cssnano                     = require('gulp-cssnano'),
+        babel                       = require('gulp-babel'),
+        webpack                     = require('webpack-stream'),
+        uglify                      = require('gulp-uglify'),
+        concat                      = require('gulp-concat'),
+        browserSync                 = require('browser-sync').create(),
 
-        folder_assets_main        = './assets/',
-        folder_src_main           = folder_assets_main + 'src/',
-        folder_dist_main          = folder_assets_main + 'dist/',
+        cfgprod                     = {production: !!util.env.production, sourceMaps: !util.env.production},
 
-        folder_node_modules       = './node_modules/',
-        folder_dist_node_modules  = folder_assets_main + 'vendor/',
+        folder_assets_main          = './assets/',
+        folder_src_main             = folder_assets_main + 'src/',
+        folder_dist_main            = folder_assets_main + 'dist/',
 
-        node_dependencies         = Object.keys(require('./package.json').dependencies || {});
+        folder_node_modules         = './node_modules/',
+        folder_dist_node_modules    = folder_assets_main + 'vendor/',
 
-/* BOOTSTRAP START */
-gulp.task('sass-bootstrap', () => {
-  return gulp.src([
-      folder_src_main + 'sass/bootstrap/**/*.scss',
-      folder_src_main + 'sass/bootstrap/**/*.sass'
-  ])
-      .pipe(sourcemaps.init())
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(autoprefixer({
-          flexbox: 'no-2009'
-      }))
-      .pipe(cssnano({
-          reduceIdents: false,
-          discardComments: {
-              removeAll: true
-          }
-      }))
-      .pipe(concat('bootstrap.css'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(folder_dist_main + 'css'))
-      .pipe(browserSync.stream());
-});
-/* BOOTSTRAP END */
+        node_dependencies           = Object.keys(require('./package.json').dependencies || {});
 
 /* MAINBUNDLE START */
 gulp.task('sass-custom', () => {
@@ -55,18 +34,20 @@ gulp.task('sass-custom', () => {
         folder_src_main + 'sass/mainpage/**/*.scss',
         folder_src_main + 'sass/mainpage/**/*.sass'
     ])
-        .pipe(sourcemaps.init())
+        .pipe(gulppif(cfgprod.sourceMaps, sourcemaps.init()))
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer({
             flexbox: 'no-2009'
         }))
-        .pipe(cssnano({
+        .pipe(cfgprod.production ? cssnano({
             reduceIdents: false,
             discardComments: {
                 removeAll: true
-            }
-        }))
+            },
+            discardUnused: false,
+            zindex: false,
+        }) : util.noop())
         .pipe(concat('custom.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(folder_dist_main + 'css'))
@@ -111,18 +92,20 @@ gulp.task('sass-client', () => {
         folder_src_main + 'sass/tradepage/**/*.scss',
         folder_src_main + 'sass/tradepage/**/*.sass'
     ])
-        .pipe(sourcemaps.init())
+        .pipe(gulppif(cfgprod.sourceMaps, sourcemaps.init()))
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer({
             flexbox: 'no-2009'
         }))
-        .pipe(cssnano({
+        .pipe(cfgprod.production ? cssnano({
             reduceIdents: false,
             discardComments: {
                 removeAll: true
-            }
-        }))
+            },
+            discardUnused: false,
+            zindex: false,
+        }) : util.noop())
         .pipe(concat('client.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(folder_dist_main + 'css'))
@@ -158,172 +141,26 @@ gulp.task('sass-xtras', () => {
         folder_src_main + 'sass/xtras/**/*.scss',
         folder_src_main + 'sass/xtras/**/*.sass'
     ])
-        .pipe(sourcemaps.init())
+        .pipe(gulppif(cfgprod.sourceMaps, sourcemaps.init()))
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer({
             flexbox: 'no-2009'
         }))
-        .pipe(cssnano({
+        .pipe(cfgprod.production ? cssnano({
             reduceIdents: false,
             discardComments: {
                 removeAll: true
-            }
-        }))
+            },
+            discardUnused: false,
+            zindex: false,
+        }) : util.noop())
         .pipe(concat('xtras.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(folder_dist_main + 'css'))
         .pipe(browserSync.stream());
 });
 /* XTRAS END */
-
-/* PLUGINS START */
-gulp.task('sass-plugin', () => {
-  return gulp.src([
-      folder_src_main + 'sass/global/**/*.scss',
-      folder_src_main + 'sass/global/**/*.sass'
-  ])
-      .pipe(sourcemaps.init())
-      .pipe(plumber())
-      .pipe(sass())
-      .pipe(autoprefixer({
-          flexbox: 'no-2009'
-      }))
-      .pipe(cssnano({
-          reduceIdents: false,
-          discardComments: {
-              removeAll: true
-          }
-      }))
-      .pipe(concat('plugin.css'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(folder_dist_main + 'css'))
-      .pipe(browserSync.stream());
-});
-
-gulp.task('script-plugin', () => {
-    return gulp.src([
-        folder_src_main + 'script/plugin/**/app.js',
-        folder_src_main + 'script/plugin/**/responsive-app.js',
-        folder_src_main + 'script/plugin/**/responsive-tabs.js',
-        folder_src_main + 'script/plugin/**/plugin-es6.js',
-        folder_src_main + 'script/plugin/**/plugin-browser-detection.js'
-    ])
-        .pipe(plumber())
-        .pipe(webpack({
-            mode: 'production'
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: [ '@babel/env' ]
-        }))
-        .pipe(concat('plugin-app.js'))
-        .pipe(uglify('plugin-app.min.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(folder_dist_main + 'js'))
-        .pipe(browserSync.stream());
-});
-/* PLUGINS END */
-
-
-/* MAINPAGE START */
-gulp.task('sass-mainpage', () => {
-    return gulp.src([
-        folder_src_main + 'sass/mainpage/**/*.scss',
-        folder_src_main + 'sass/mainpage/**/*.sass'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            flexbox: 'no-2009'
-        }))
-        .pipe(cssnano({
-            reduceIdents: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
-        .pipe(concat('mainpage-app.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(folder_dist_main + 'css'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('script-mainpage', () => {
-    return gulp.src([
-        folder_src_main + 'script/mainpage/**/app.js',
-        folder_src_main + 'script/mainpage/**/validations-homepage-register.js',
-        folder_src_main + 'script/mainpage/**/validations-signup.js',
-        folder_src_main + 'script/mainpage/**/validations-login.js',
-        folder_src_main + 'script/mainpage/**/validations-contact.js',
-        folder_src_main + 'script/mainpage/**/validations-call2action.js',
-        folder_src_main + 'script/mainpage/**/responsive-app.js'
-    ])
-        .pipe(plumber())
-        .pipe(webpack({
-            mode: 'production'
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: [ '@babel/env' ]
-        }))
-        .pipe(concat('mainpage-app.js'))
-        .pipe(uglify('mainpage-app.min.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(folder_dist_main + 'js'))
-        .pipe(browserSync.stream());
-});
-/* MAINPAGE END */
-
-
-/* TRADEPAGE START */
-gulp.task('sass-tradepage', () => {
-    return gulp.src([
-        folder_src_main + 'sass/tradepage/**/*.scss',
-        folder_src_main + 'sass/tradepage/**/*.sass'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(autoprefixer({
-            flexbox: 'no-2009'
-        }))
-        .pipe(cssnano({
-            reduceIdents: false,
-            discardComments: {
-                removeAll: true
-            }
-        }))
-        .pipe(concat('tradepage-app.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(folder_dist_main + 'css'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('script-tradepage', () => {
-    return gulp.src([
-        folder_src_main + 'script/tradepage/**/app.js',
-        folder_src_main + 'script/tradepage/**/deposit-withdraw.js',
-        folder_src_main + 'script/tradepage/**/validations-profile-login-settings.js',
-        folder_src_main + 'script/tradepage/**/responsive-app.js'
-    ])
-        .pipe(plumber())
-        .pipe(webpack({
-            mode: 'production'
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: [ '@babel/env' ]
-        }))
-        .pipe(concat('tradepage-app.js'))
-        .pipe(uglify('tradepage-app.min.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(folder_dist_main + 'js'))
-        .pipe(browserSync.stream());
-});
-/* TRADEPAGE END */
-
 
 /* GENERAL START */
 gulp.task('clear', () => del([ folder_dist_main, folder_dist_node_modules ]));
@@ -394,16 +231,9 @@ gulp.task('watch', () => {
 
 
 /* GULP START */
-gulp.task('bootstrap', gulp.series('sass-bootstrap'));
-gulp.task('plugin', gulp.series('sass-plugin', 'script-plugin'));
-gulp.task('mainpage', gulp.series('sass-mainpage', 'script-mainpage'));
-gulp.task('tradepage', gulp.series('sass-tradepage', 'script-tradepage'));
 gulp.task('custom', gulp.series('sass-custom', 'script-custom'));
 gulp.task('client', gulp.series('sass-client', 'script-client'));
 gulp.task('xtras', gulp.series('sass-xtras'));
-
-// gulp.task('build', gulp.series('clear', 'vendor', 'bootstrap', 'plugin', 'mainpage', 'tradepage', 'custom', 'client', 'xtras'));
-// gulp.task('devel', gulp.series('bootstrap', 'plugin', 'mainpage', 'tradepage', 'custom', 'client', 'xtras', gulp.parallel('watch')));
 
 gulp.task('build', gulp.series('clear', 'vendor', 'custom', 'client', 'xtras'));
 gulp.task('devel', gulp.series('custom', 'client', 'xtras', gulp.parallel('watch')));
